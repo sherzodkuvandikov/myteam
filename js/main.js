df4e833047947fbb18e08b42d03d9f15b8c8ef2d
MuhammadSiddiq <707myworld@gmail.com>
2020-10-11 12:49:59 +0500

    add hamburger menu part

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-function openHeaderMenyu() {
-  document.getElementById("headerMenyu").style.width = "255px";
-}
+// function openHeaderMenyu() {
+//   document.getElementById("headerMenyu").style.width = "255px";
+// }
 
-function closeHeaderMenyu() {
-  document.getElementById("headerMenyu").style.width = "0";
-}
+// function closeHeaderMenyu() {
+//   document.getElementById("headerMenyu").style.width = "0";
+// }
+
+const hamburger = document.querySelector(".sitenav__toggler");
+const navmenu = document.querySelector(".sitenav__link-wrapper");
+
+
+hamburger.addEventListener("click", () => {
+    navmenu.classList.toggle("sitenav__link-wrapper--open");
+})
